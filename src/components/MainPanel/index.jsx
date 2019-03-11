c86a28b51e64a89d650ffbd99ef490eca5fc39d8
KyleBurney <k.burney323@gmail.com>
2019-03-10 17:06:55 -0700

    lesson 2.3

--- src/components/MainPanel/index.jsx
+++ src/components/MainPanel/index.jsx
@@ -9,31 +9,35 @@ import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
 import newerWords from '../../data/newerwords.json';
 import memorizedWords from '../../data/memorizedwords.json';
-import lesson1Words from '../../data/lesson1.json';
-import lesson2Words from '../../data/lesson2.json';
-import lesson3Words from '../../data/lesson3.json';
-import lesson4Words from '../../data/lesson4.json';
-import lesson5Words from '../../data/lesson5.json';
-import lesson6Words from '../../data/lesson6.json';
-import lesson7Words from '../../data/lesson7.json';
-import lesson8Words from '../../data/lesson8.json';
-import lesson9Words from '../../data/lesson9.json';
-import lesson10Words from '../../data/lesson10.json';
-import lesson11Words from '../../data/lesson11.json';
-import lesson12Words from '../../data/lesson12.json';
-import lesson13Words from '../../data/lesson13.json';
-import lesson14Words from '../../data/lesson14.json';
-import lesson15Words from '../../data/lesson15.json';
-import lesson16Words from '../../data/lesson16.json';
-import lesson17Words from '../../data/lesson17.json';
-import lesson18Words from '../../data/lesson18.json';
-import lesson19Words from '../../data/lesson19.json';
-import lesson20Words from '../../data/lesson20.json';
-import lesson21Words from '../../data/lesson21.json';
-import lesson22Words from '../../data/lesson22.json';
-import lesson23Words from '../../data/lesson23.json';
-import lesson24Words from '../../data/lesson24.json';
-import lesson25Words from '../../data/lesson25.json';
+import song1Words from '../../data/songs/song1.json';
+import lesson1_1Words from '../../data/lesson1/lesson1.json';
+import lesson1_2Words from '../../data/lesson1/lesson2.json';
+import lesson1_3Words from '../../data/lesson1/lesson3.json';
+import lesson1_4Words from '../../data/lesson1/lesson4.json';
+import lesson1_5Words from '../../data/lesson1/lesson5.json';
+import lesson1_6Words from '../../data/lesson1/lesson6.json';
+import lesson1_7Words from '../../data/lesson1/lesson7.json';
+import lesson1_8Words from '../../data/lesson1/lesson8.json';
+import lesson1_9Words from '../../data/lesson1/lesson9.json';
+import lesson1_10Words from '../../data/lesson1/lesson10.json';
+import lesson1_11Words from '../../data/lesson1/lesson11.json';
+import lesson1_12Words from '../../data/lesson1/lesson12.json';
+import lesson1_13Words from '../../data/lesson1/lesson13.json';
+import lesson1_14Words from '../../data/lesson1/lesson14.json';
+import lesson1_15Words from '../../data/lesson1/lesson15.json';
+import lesson1_16Words from '../../data/lesson1/lesson16.json';
+import lesson1_17Words from '../../data/lesson1/lesson17.json';
+import lesson1_18Words from '../../data/lesson1/lesson18.json';
+import lesson1_19Words from '../../data/lesson1/lesson19.json';
+import lesson1_20Words from '../../data/lesson1/lesson20.json';
+import lesson1_21Words from '../../data/lesson1/lesson21.json';
+import lesson1_22Words from '../../data/lesson1/lesson22.json';
+import lesson1_23Words from '../../data/lesson1/lesson23.json';
+import lesson1_24Words from '../../data/lesson1/lesson24.json';
+import lesson1_25Words from '../../data/lesson1/lesson25.json';
+import lesson2_1Words from '../../data/lesson2/lesson1.json';
+import lesson2_2Words from '../../data/lesson2/lesson2.json';
+import lesson2_3Words from '../../data/lesson2/lesson3.json';
 
 const styles = theme => ({
   paper: {
@@ -88,31 +92,35 @@ class MainPanel extends Component {
       <div className="MainPanel">
         <Button color={this.state.cardsToShow === newerWords ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: newerWords, showAllCards: true }) }}>New cards</Button>
         <Button color={this.state.cardsToShow === memorizedWords ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: memorizedWords, showAllCards: true }) }}>Old cards</Button>
-        <Button color={this.state.cardsToShow === lesson1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1Words, showAllCards: true }) }}>Lesson 1</Button>
-        <Button color={this.state.cardsToShow === lesson2Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2Words, showAllCards: true }) }}>Lesson 2</Button>
-        <Button color={this.state.cardsToShow === lesson3Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson3Words, showAllCards: true }) }}>Lesson 3</Button>
-        <Button color={this.state.cardsToShow === lesson4Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson4Words, showAllCards: true }) }}>Lesson 4</Button>
-        <Button color={this.state.cardsToShow === lesson5Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson5Words, showAllCards: true }) }}>Lesson 5</Button>
-        <Button color={this.state.cardsToShow === lesson6Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson6Words, showAllCards: true }) }}>Lesson 6</Button>
-        <Button color={this.state.cardsToShow === lesson7Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson7Words, showAllCards: true }) }}>Lesson 7</Button>
-        <Button color={this.state.cardsToShow === lesson8Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson8Words, showAllCards: true }) }}>Lesson 8</Button>
-        <Button color={this.state.cardsToShow === lesson9Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson9Words, showAllCards: true }) }}>Lesson 9</Button>
-        <Button color={this.state.cardsToShow === lesson10Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson10Words, showAllCards: true }) }}>Lesson 10</Button>
-        <Button color={this.state.cardsToShow === lesson11Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson11Words, showAllCards: true }) }}>Lesson 11</Button>
-        <Button color={this.state.cardsToShow === lesson12Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson12Words, showAllCards: true }) }}>Lesson 12</Button>
-        <Button color={this.state.cardsToShow === lesson13Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson13Words, showAllCards: true }) }}>Lesson 13</Button>
-        <Button color={this.state.cardsToShow === lesson14Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson14Words, showAllCards: true }) }}>Lesson 14</Button>
-        <Button color={this.state.cardsToShow === lesson15Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson15Words, showAllCards: true }) }}>Lesson 15</Button>
-        <Button color={this.state.cardsToShow === lesson16Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson16Words, showAllCards: true }) }}>Lesson 16</Button>
-        <Button color={this.state.cardsToShow === lesson17Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson17Words, showAllCards: true }) }}>Lesson 17</Button>
-        <Button color={this.state.cardsToShow === lesson18Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson18Words, showAllCards: true }) }}>Lesson 18</Button>
-        <Button color={this.state.cardsToShow === lesson19Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson19Words, showAllCards: true }) }}>Lesson 19</Button>
-        <Button color={this.state.cardsToShow === lesson20Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson20Words, showAllCards: true }) }}>Lesson 20</Button>
-        <Button color={this.state.cardsToShow === lesson21Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson21Words, showAllCards: true }) }}>Lesson 21</Button>
-        <Button color={this.state.cardsToShow === lesson22Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson22Words, showAllCards: true }) }}>Lesson 22</Button>
-        <Button color={this.state.cardsToShow === lesson23Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson23Words, showAllCards: true }) }}>Lesson 23</Button>
-        <Button color={this.state.cardsToShow === lesson24Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson24Words, showAllCards: true }) }}>Lesson 24</Button>
-        <Button color={this.state.cardsToShow === lesson25Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson25Words, showAllCards: true }) }}>Lesson 25</Button>
+        <Button color={this.state.cardsToShow === song1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: song1Words, showAllCards: true }) }}>S1</Button>
+        <Button color={this.state.cardsToShow === lesson1_1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_1Words, showAllCards: true }) }}>1.1</Button>
+        <Button color={this.state.cardsToShow === lesson1_2Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_2Words, showAllCards: true }) }}>1.2</Button>
+        <Button color={this.state.cardsToShow === lesson1_3Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_3Words, showAllCards: true }) }}>1.3</Button>
+        <Button color={this.state.cardsToShow === lesson1_4Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_4Words, showAllCards: true }) }}>1.4</Button>
+        <Button color={this.state.cardsToShow === lesson1_5Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_5Words, showAllCards: true }) }}>1.5</Button>
+        <Button color={this.state.cardsToShow === lesson1_6Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_6Words, showAllCards: true }) }}>1.6</Button>
+        <Button color={this.state.cardsToShow === lesson1_7Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_7Words, showAllCards: true }) }}>1.7</Button>
+        <Button color={this.state.cardsToShow === lesson1_8Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_8Words, showAllCards: true }) }}>1.8</Button>
+        <Button color={this.state.cardsToShow === lesson1_9Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_9Words, showAllCards: true }) }}>1.9</Button>
+        <Button color={this.state.cardsToShow === lesson1_10Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_10Words, showAllCards: true }) }}>1.10</Button>
+        <Button color={this.state.cardsToShow === lesson1_11Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_11Words, showAllCards: true }) }}>1.11</Button>
+        <Button color={this.state.cardsToShow === lesson1_12Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_12Words, showAllCards: true }) }}>1.12</Button>
+        <Button color={this.state.cardsToShow === lesson1_13Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_13Words, showAllCards: true }) }}>1.13</Button>
+        <Button color={this.state.cardsToShow === lesson1_14Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_14Words, showAllCards: true }) }}>1.14</Button>
+        <Button color={this.state.cardsToShow === lesson1_15Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_15Words, showAllCards: true }) }}>1.15</Button>
+        <Button color={this.state.cardsToShow === lesson1_16Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_16Words, showAllCards: true }) }}>1.16</Button>
+        <Button color={this.state.cardsToShow === lesson1_17Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_17Words, showAllCards: true }) }}>1.17</Button>
+        <Button color={this.state.cardsToShow === lesson1_18Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_18Words, showAllCards: true }) }}>1.18</Button>
+        <Button color={this.state.cardsToShow === lesson1_19Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_19Words, showAllCards: true }) }}>1.19</Button>
+        <Button color={this.state.cardsToShow === lesson1_20Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_20Words, showAllCards: true }) }}>1.20</Button>
+        <Button color={this.state.cardsToShow === lesson1_21Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_21Words, showAllCards: true }) }}>1.21</Button>
+        <Button color={this.state.cardsToShow === lesson1_22Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_22Words, showAllCards: true }) }}>1.22</Button>
+        <Button color={this.state.cardsToShow === lesson1_23Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_23Words, showAllCards: true }) }}>1.23</Button>
+        <Button color={this.state.cardsToShow === lesson1_24Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_24Words, showAllCards: true }) }}>1.24</Button>
+        <Button color={this.state.cardsToShow === lesson1_25Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_25Words, showAllCards: true }) }}>1.25</Button>
+        <Button color={this.state.cardsToShow === lesson2_1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_1Words, showAllCards: true }) }}>2.1</Button>
+        <Button color={this.state.cardsToShow === lesson2_2Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_2Words, showAllCards: true }) }}>2.2</Button>
+        <Button color={this.state.cardsToShow === lesson2_3Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_3Words, showAllCards: true }) }}>2.3</Button>
         <Button onClick={() => { this.setState({ showAllCards: false }) }}>One card at a time</Button>
         <Button onClick={this.handleOpen}>Add a Card</Button>
         <Modal
