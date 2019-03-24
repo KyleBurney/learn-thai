56dfdfdbf58e5d9e1fa79ede0437e32b2828a0c3
KyleBurney <k.burney323@gmail.com>
2019-03-24 10:47:28 -0700

    adding dropdowns and one-card-at-a-time update

--- src/components/CardsPanel/index.jsx
+++ src/components/CardsPanel/index.jsx
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import FlashCard from '../FlashCard';
 import Button from '@material-ui/core/Button';
-import newerWords from '../../data/newerwords.json';
-import memorizedWords from '../../data/memorizedwords.json';
-
-var words = newerWords.concat(memorizedWords);
 
 class CardsPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
             wordNum: 0,
-            remainingWords: Object.assign([], words),
-            selectedWord: words[0],
+            remainingWords: Object.assign([], props.words),
+            selectedWord: props.words[0],
             currentIndex: 0
         }
     }
 
-    componentWillMount() {
-        for (let i = words.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [words[i], words[j]] = [words[j], words[i]];
-        }
-        this.setState({ cards: words });
-    }
+    // componentWillMount() {
+    //     for (let i = this.props.words.length - 1; i > 0; i--) {
+    //         const j = Math.floor(Math.random() * (i + 1));
+    //         [this.props.words[i], this.props.words[j]] = [this.props.words[j], this.props.words[i]];
+    //     }
+    //     this.setState({ cards: this.props.words });
+    // }
+
     componentDidMount() {
         document.addEventListener("keydown", this.handleKeyPress, false);
     }
 
     componentWillUnmount() {
         document.removeEventListener("keydown", this.handleKeyPress, false);
+    }
+
+    shuffle = () => {
+        var shuffledCards = this.props.words;
+        for (let i = shuffledCards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+        }
+        this.setState({ cards: shuffledCards });
     }
 
     handleKeyPress = event => {
@@ -41,7 +47,7 @@ class CardsPanel extends Component {
     }
 
     moveRight = () => {
-        if (this.state.currentIndex < words.length - 1) {
+        if (this.state.currentIndex < this.props.words.length - 1) {
             this.setState({ currentIndex: this.state.currentIndex + 1 })
         }
     }
@@ -55,11 +61,14 @@ class CardsPanel extends Component {
     render() {
         return (
             <div className="MainPanel">
-                <FlashCard thai={words[this.state.currentIndex].thai} english={words[this.state.currentIndex].english} definition={words[this.state.currentIndex].definition} filename={words[this.state.currentIndex].filename} dateAdded={words[this.state.currentIndex].dateAdded} />
+                <Button onClick={() => {
+                    this.shuffle()
+                }}>Shuffle cards</Button>
+                <FlashCard thai={this.props.words[this.state.currentIndex].thai} english={this.props.words[this.state.currentIndex].english} definition={this.props.words[this.state.currentIndex].definition} filename={this.props.words[this.state.currentIndex].filename} dateAdded={this.props.words[this.state.currentIndex].dateAdded} />
 
                 <Button variant="outlined" onClick={this.moveLeft}>Back</Button>
                 <Button variant="outlined" onClick={this.moveRight}>Next</Button>
-                {1 + this.state.currentIndex} / {words.length}
+                {1 + this.state.currentIndex} / {this.props.words.length}
             </div>
         );
     }
--- src/components/MainPanel/index.jsx
+++ src/components/MainPanel/index.jsx
@@ -7,9 +7,16 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import newerWords from '../../data/newerwords.json';
 import memorizedWords from '../../data/memorizedwords.json';
 import song1Words from '../../data/songs/song1.json';
+import song1fullWords from '../../data/songs/song1full.json';
 import lesson1_1Words from '../../data/lesson1/lesson1.json';
 import lesson1_2Words from '../../data/lesson1/lesson2.json';
 import lesson1_3Words from '../../data/lesson1/lesson3.json';
@@ -38,6 +45,7 @@ import lesson1_25Words from '../../data/lesson1/lesson25.json';
 import lesson2_1Words from '../../data/lesson2/lesson1.json';
 import lesson2_2Words from '../../data/lesson2/lesson2.json';
 import lesson2_3Words from '../../data/lesson2/lesson3.json';
+import lesson2_4Words from '../../data/lesson2/lesson4.json';
 
 const styles = theme => ({
   paper: {
@@ -50,6 +58,20 @@ const styles = theme => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
   },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing.unit * 2
+  },
+  selectedGroup: {
+    color: "red"
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  },
+  root: {
+    flexGrow: 1
+  }
 });
 
 class MainPanel extends Component {
@@ -58,7 +80,10 @@ class MainPanel extends Component {
     this.state = {
       showAllCards: true,
       cardsToShow: newerWords,
-      open: false
+      openModal: false,
+      openGeneralSelection: false,
+      openSongsSelection: false,
+      openLessonsSelection: false
     }
   }
 
@@ -77,65 +102,128 @@ class MainPanel extends Component {
     return 0;
   }
 
-  handleOpen = () => {
-    this.setState({ open: true });
+  handleOpenModal = () => {
+    this.setState({ openModal: true });
   };
 
-  handleClose = () => {
-    this.setState({ open: false });
+  handleCloseModal = () => {
+    this.setState({ openModal: false });
+  };
+
+  handleChangeSelection = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleCloseSelection = (openSelectionGroup) => {
+    this.setState({ [openSelectionGroup]: false });
+  };
+
+  handleOpenSelection = (openSelectionGroup) => {
+    console.log(openSelectionGroup)
+    this.setState({ [openSelectionGroup]: true });
   };
 
   render() {
     const { classes } = this.props;
 
     return (
-      <div className="MainPanel">
-        <Button color={this.state.cardsToShow === newerWords ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: newerWords, showAllCards: true }) }}>New cards</Button>
-        <Button color={this.state.cardsToShow === memorizedWords ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: memorizedWords, showAllCards: true }) }}>Old cards</Button>
-        <Button color={this.state.cardsToShow === song1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: song1Words, showAllCards: true }) }}>S1</Button>
-        <Button color={this.state.cardsToShow === lesson1_1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_1Words, showAllCards: true }) }}>1.1</Button>
-        <Button color={this.state.cardsToShow === lesson1_2Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_2Words, showAllCards: true }) }}>1.2</Button>
-        <Button color={this.state.cardsToShow === lesson1_3Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_3Words, showAllCards: true }) }}>1.3</Button>
-        <Button color={this.state.cardsToShow === lesson1_4Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_4Words, showAllCards: true }) }}>1.4</Button>
-        <Button color={this.state.cardsToShow === lesson1_5Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_5Words, showAllCards: true }) }}>1.5</Button>
-        <Button color={this.state.cardsToShow === lesson1_6Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_6Words, showAllCards: true }) }}>1.6</Button>
-        <Button color={this.state.cardsToShow === lesson1_7Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_7Words, showAllCards: true }) }}>1.7</Button>
-        <Button color={this.state.cardsToShow === lesson1_8Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_8Words, showAllCards: true }) }}>1.8</Button>
-        <Button color={this.state.cardsToShow === lesson1_9Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_9Words, showAllCards: true }) }}>1.9</Button>
-        <Button color={this.state.cardsToShow === lesson1_10Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_10Words, showAllCards: true }) }}>1.10</Button>
-        <Button color={this.state.cardsToShow === lesson1_11Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_11Words, showAllCards: true }) }}>1.11</Button>
-        <Button color={this.state.cardsToShow === lesson1_12Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_12Words, showAllCards: true }) }}>1.12</Button>
-        <Button color={this.state.cardsToShow === lesson1_13Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_13Words, showAllCards: true }) }}>1.13</Button>
-        <Button color={this.state.cardsToShow === lesson1_14Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_14Words, showAllCards: true }) }}>1.14</Button>
-        <Button color={this.state.cardsToShow === lesson1_15Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_15Words, showAllCards: true }) }}>1.15</Button>
-        <Button color={this.state.cardsToShow === lesson1_16Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_16Words, showAllCards: true }) }}>1.16</Button>
-        <Button color={this.state.cardsToShow === lesson1_17Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_17Words, showAllCards: true }) }}>1.17</Button>
-        <Button color={this.state.cardsToShow === lesson1_18Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_18Words, showAllCards: true }) }}>1.18</Button>
-        <Button color={this.state.cardsToShow === lesson1_19Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_19Words, showAllCards: true }) }}>1.19</Button>
-        <Button color={this.state.cardsToShow === lesson1_20Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_20Words, showAllCards: true }) }}>1.20</Button>
-        <Button color={this.state.cardsToShow === lesson1_21Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_21Words, showAllCards: true }) }}>1.21</Button>
-        <Button color={this.state.cardsToShow === lesson1_22Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_22Words, showAllCards: true }) }}>1.22</Button>
-        <Button color={this.state.cardsToShow === lesson1_23Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_23Words, showAllCards: true }) }}>1.23</Button>
-        <Button color={this.state.cardsToShow === lesson1_24Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_24Words, showAllCards: true }) }}>1.24</Button>
-        <Button color={this.state.cardsToShow === lesson1_25Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson1_25Words, showAllCards: true }) }}>1.25</Button>
-        <Button color={this.state.cardsToShow === lesson2_1Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_1Words, showAllCards: true }) }}>2.1</Button>
-        <Button color={this.state.cardsToShow === lesson2_2Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_2Words, showAllCards: true }) }}>2.2</Button>
-        <Button color={this.state.cardsToShow === lesson2_3Words ? "secondary" : ""} onClick={() => { this.setState({ cardsToShow: lesson2_3Words, showAllCards: true }) }}>2.3</Button>
+      <div>
+        <form className={classes.root} autoComplete="off">
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="card-group-general-select">General Words</InputLabel>
+            <Select
+              open={this.state.openGeneralSelection}
+              onClose={() => this.handleCloseSelection("openGeneralSelection")}
+              onOpen={() => this.handleOpenSelection("openGeneralSelection")}
+              value={this.state.cardsToShow}
+              onChange={this.handleChangeSelection}
+              inputProps={{
+                name: 'cardsToShow',
+                id: 'card-group-general-select',
+              }}
+            >
+              <MenuItem value={newerWords}><span className={this.state.cardsToShow === newerWords ? classes.selectedGroup : ""}>New Words</span></MenuItem>
+              <MenuItem value={memorizedWords}><span className={this.state.cardsToShow === memorizedWords ? classes.selectedGroup : ""}>Memorized Words</span></MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="card-group-songs-select">Words From Songs</InputLabel>
+            <Select
+              open={this.state.openSongsSelection}
+              onClose={() => this.handleCloseSelection("openSongsSelection")}
+              onOpen={() => this.handleOpenSelection("openSongsSelection")}
+              value={this.state.cardsToShow}
+              onChange={this.handleChangeSelection}
+              inputProps={{
+                name: 'cardsToShow',
+                id: 'card-group-songs-select',
+              }}
+            >
+              <MenuItem value={song1Words}><span className={this.state.cardsToShow === song1Words ? classes.selectedGroup : ""}>Song 1 New Words</span></MenuItem>
+              <MenuItem value={song1fullWords}><span className={this.state.cardsToShow === song1fullWords ? classes.selectedGroup : ""}>Song 1 All Words</span></MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="card-group-lessons-select">Words From Lessons</InputLabel>
+            <Select
+              open={this.state.openLessonsSelection}
+              onClose={() => this.handleCloseSelection("openLessonsSelection")}
+              onOpen={() => this.handleOpenSelection("openLessonsSelection")}
+              value={this.state.cardsToShow}
+              onChange={this.handleChangeSelection}
+              inputProps={{
+                name: 'cardsToShow',
+                id: 'card-group-lessons-select',
+              }}
+            >
+              <MenuItem value={lesson1_1Words}><span className={this.state.cardsToShow === lesson1_1Words ? classes.selectedGroup : ""}>lesson 1.1 Words</span></MenuItem>
+              <MenuItem value={lesson1_2Words}><span className={this.state.cardsToShow === lesson1_2Words ? classes.selectedGroup : ""}>lesson 1.2 Words</span></MenuItem>
+              <MenuItem value={lesson1_3Words}><span className={this.state.cardsToShow === lesson1_3Words ? classes.selectedGroup : ""}>lesson 1.3 Words</span></MenuItem>
+              <MenuItem value={lesson1_4Words}><span className={this.state.cardsToShow === lesson1_4Words ? classes.selectedGroup : ""}>lesson 1.4 Words</span></MenuItem>
+              <MenuItem value={lesson1_5Words}><span className={this.state.cardsToShow === lesson1_5Words ? classes.selectedGroup : ""}>lesson 1.5 Words</span></MenuItem>
+              <MenuItem value={lesson1_6Words}><span className={this.state.cardsToShow === lesson1_6Words ? classes.selectedGroup : ""}>lesson 1.6 Words</span></MenuItem>
+              <MenuItem value={lesson1_7Words}><span className={this.state.cardsToShow === lesson1_7Words ? classes.selectedGroup : ""}>lesson 1.7 Words</span></MenuItem>
+              <MenuItem value={lesson1_8Words}><span className={this.state.cardsToShow === lesson1_8Words ? classes.selectedGroup : ""}>lesson 1.8 Words</span></MenuItem>
+              <MenuItem value={lesson1_9Words}><span className={this.state.cardsToShow === lesson1_9Words ? classes.selectedGroup : ""}>lesson 1.9 Words</span></MenuItem>
+              <MenuItem value={lesson1_10Words}><span className={this.state.cardsToShow === lesson1_10Words ? classes.selectedGroup : ""}>lesson 1.10 Words</span></MenuItem>
+              <MenuItem value={lesson1_11Words}><span className={this.state.cardsToShow === lesson1_11Words ? classes.selectedGroup : ""}>lesson 1.11 Words</span></MenuItem>
+              <MenuItem value={lesson1_12Words}><span className={this.state.cardsToShow === lesson1_12Words ? classes.selectedGroup : ""}>lesson 1.12 Words</span></MenuItem>
+              <MenuItem value={lesson1_13Words}><span className={this.state.cardsToShow === lesson1_13Words ? classes.selectedGroup : ""}>lesson 1.13 Words</span></MenuItem>
+              <MenuItem value={lesson1_14Words}><span className={this.state.cardsToShow === lesson1_14Words ? classes.selectedGroup : ""}>lesson 1.14 Words</span></MenuItem>
+              <MenuItem value={lesson1_15Words}><span className={this.state.cardsToShow === lesson1_15Words ? classes.selectedGroup : ""}>lesson 1.15 Words</span></MenuItem>
+              <MenuItem value={lesson1_16Words}><span className={this.state.cardsToShow === lesson1_16Words ? classes.selectedGroup : ""}>lesson 1.16 Words</span></MenuItem>
+              <MenuItem value={lesson1_17Words}><span className={this.state.cardsToShow === lesson1_17Words ? classes.selectedGroup : ""}>lesson 1.17 Words</span></MenuItem>
+              <MenuItem value={lesson1_18Words}><span className={this.state.cardsToShow === lesson1_18Words ? classes.selectedGroup : ""}>lesson 1.18 Words</span></MenuItem>
+              <MenuItem value={lesson1_19Words}><span className={this.state.cardsToShow === lesson1_19Words ? classes.selectedGroup : ""}>lesson 1.19 Words</span></MenuItem>
+              <MenuItem value={lesson1_20Words}><span className={this.state.cardsToShow === lesson1_20Words ? classes.selectedGroup : ""}>lesson 1.20 Words</span></MenuItem>
+              <MenuItem value={lesson1_21Words}><span className={this.state.cardsToShow === lesson1_21Words ? classes.selectedGroup : ""}>lesson 1.21 Words</span></MenuItem>
+              <MenuItem value={lesson1_22Words}><span className={this.state.cardsToShow === lesson1_22Words ? classes.selectedGroup : ""}>lesson 1.22 Words</span></MenuItem>
+              <MenuItem value={lesson1_23Words}><span className={this.state.cardsToShow === lesson1_23Words ? classes.selectedGroup : ""}>lesson 1.23 Words</span></MenuItem>
+              <MenuItem value={lesson1_24Words}><span className={this.state.cardsToShow === lesson1_24Words ? classes.selectedGroup : ""}>lesson 1.24 Words</span></MenuItem>
+              <MenuItem value={lesson1_25Words}><span className={this.state.cardsToShow === lesson1_25Words ? classes.selectedGroup : ""}>lesson 1.25 Words</span></MenuItem>
+              <MenuItem value={lesson2_1Words}><span className={this.state.cardsToShow === lesson2_1Words ? classes.selectedGroup : ""}>lesson 2.1 Words</span></MenuItem>
+              <MenuItem value={lesson2_2Words}><span className={this.state.cardsToShow === lesson2_2Words ? classes.selectedGroup : ""}>lesson 2.2 Words</span></MenuItem>
+              <MenuItem value={lesson2_3Words}><span className={this.state.cardsToShow === lesson2_3Words ? classes.selectedGroup : ""}>lesson 2.3 Words</span></MenuItem>
+              <MenuItem value={lesson2_4Words}><span className={this.state.cardsToShow === lesson2_4Words ? classes.selectedGroup : ""}>lesson 2.4 Words</span></MenuItem>
+            </Select>
+          </FormControl>
+        </form>
         <Button onClick={() => { this.setState({ showAllCards: false }) }}>One card at a time</Button>
-        <Button onClick={this.handleOpen}>Add a Card</Button>
+        <Button onClick={() => { this.setState({ showAllCards: true }) }}>All cards at once</Button>
+        <Button onClick={this.handleOpenModal}>Add a Card</Button>
         <Modal
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
-          open={this.state.open}
-          onClose={this.handleClose}
+          open={this.state.openModal}
+          onClose={this.handleCloseModal}
         >
           <div className={classes.paper}>
             <AddCardPanel />
           </div>
         </Modal>
-        {this.state.showAllCards ? <MultiCardsPanel words={this.state.cardsToShow} /> : <CardsPanel />}
+        {this.state.showAllCards ? <MultiCardsPanel words={this.state.cardsToShow} /> : <CardsPanel words={this.state.cardsToShow} />}
 
-      </div>
+      </div >
     );
   }
 }
